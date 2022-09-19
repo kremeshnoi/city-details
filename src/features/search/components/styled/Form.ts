67dd1fef1fd84aa33e65cd7226816fd6a31c0d7c
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-09-19 16:46:33 +0300

    feat: create Form styled component

---
+++ src/features/search/components/styled/Form.ts
@@ -0,0 +1,10 @@
+import { styled } from '@mui/system'
+
+const Form = styled('form')(() => ({
+	display: 'flex',
+	flexDirection: 'column',
+	margin: '40px 0 40px 0',
+	boxSizing: 'border-box',
+}))
+
+export default Form
