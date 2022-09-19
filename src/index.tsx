520bbf372a4f2d6b9bfef280f4c61d2fbeb957cc
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2022-09-19 16:42:16 +0300

    feat: adjust BrowserRouter to App

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import './index.css'
+import App from './App'
 
-const container = document.getElementById('root')!;
-const root = createRoot(container);
+import React from 'react'
+import { index } from './store'
+import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter } from 'react-router-dom'
+
+const container = document.getElementById('root')!
+const root = createRoot(container)
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
-);
+	<React.StrictMode>
+		<Provider store={index}>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</Provider>
+	</React.StrictMode>
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
